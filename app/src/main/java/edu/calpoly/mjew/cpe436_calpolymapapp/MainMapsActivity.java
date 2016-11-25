e5f3aab74207e09f325b9efc52d7c0b600d1601a
MackDaddyJew <mack.jew@gmail.com>
2016-11-24 19:31:05 -0800

    Fiddled W/activity_main_maps.xml And manifest file
    I experimented with the layout file in activity_main_maps.xml to figure out how to include 2 fragments at once. I believe I now have an idea of how to make it work. Also changed some string resource references around in the manifest to make the app name more specific. I'm not certain what the Android: Label element actually is for.
    
    -MacKenzie

--- app/src/main/java/edu/calpoly/mjew/cpe436_calpolymapapp/MainMapsActivity.java
+++ app/src/main/java/edu/calpoly/mjew/cpe436_calpolymapapp/MainMapsActivity.java
@@ -1,6 +1,7 @@
 package edu.calpoly.mjew.cpe436_calpolymapapp;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -24,13 +25,14 @@ public class MainMapsActivity extends AppCompatActivity implements OnMapReadyCal
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_maps);
         Toolbar tb = (Toolbar) findViewById(R.id.toolbar);
-        tb.setTitle(getTitle());
+        tb.setTitle(R.string.title_activity_main_maps);
         setSupportActionBar(null);
         setSupportActionBar(tb);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        //Fragment f = getSupportFragmentManager().findFragmentById(R.id.extraFrag);
     }
 
     /**
