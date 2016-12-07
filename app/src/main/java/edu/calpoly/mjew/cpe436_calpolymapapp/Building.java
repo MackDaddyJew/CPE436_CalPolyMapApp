78832f76899422dcb0f4ba58424fa96b27019133
Ben Carter <becarter@calpoly.edu>
2016-12-06 23:50:17 -0800

    Firebase update
    continued making updates to Firebase integration
    currently, loaded all buildings and their basic info into database
    going to pull information down and use it fill building fragment - BC 12-6

--- app/src/main/java/edu/calpoly/mjew/cpe436_calpolymapapp/Building.java
+++ app/src/main/java/edu/calpoly/mjew/cpe436_calpolymapapp/Building.java
@@ -1,52 +1,106 @@
 package edu.calpoly.mjew.cpe436_calpolymapapp;
 
+import android.content.ClipData;
+import android.content.res.Resources;
+import android.util.Log;
+
 import com.google.android.gms.maps.model.LatLng;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by mackenzie on 11/29/16.
  */
 public class Building
 {
+    private String mBuildingNumber;
     private String mBuildingName;
     private String mBuildingDescription;
-    private LatLng mBuildingCenter;
+    private transient LatLng mBuildingCenter;           // TODO change from transient as they are implemented
 
-    private ArrayList<ClassRoom> mAllClassRooms;
-    private ArrayList<Route> mAllRoutes;
-    private ArrayList<String> mAllBuildingPhotos;           // contains String Paths
+    private transient ArrayList<ClassRoom> mAllClassRooms;
+    private transient ArrayList<Route> mAllRoutes;
+    private transient ArrayList<String> mAllBuildingPhotos;           // contains String Paths
 
-    public Building(String bName, String bDescription, LatLng bCenter)
+
+    public Building(String bNumber, String bName, String bDescription) //, LatLng bCenter) TODO: add this param later
     {
+        mBuildingNumber = bNumber;
         mBuildingName = bName;
         mBuildingDescription = bDescription;
-        mBuildingCenter = bCenter;
+        mBuildingCenter = null; //bCenter;
 
         mAllClassRooms = new ArrayList<>();
         mAllBuildingPhotos = new ArrayList<>();
         mAllRoutes = new ArrayList<>();
     }
 
+    @Override
+    public String toString() {
+        return("Building Number: " + this.mBuildingNumber + ", "
+                + "Building Name: " + this.mBuildingName + ", "
+                + "Description: " + this.mBuildingDescription + ", ");
+
+        // TODO add rest of fields to toString
+    }
+
+    // initialize database with buildings
+    //      only ever need to call this once
+    // TODO: maybe move into a BuildingHandler class?
+    static public void InitializeAllBuildings(String[] allBuildingNames)
+    {
+        ArrayList<Building> allBuildings = new ArrayList<>();
+        Building newBuild;
+        String bName, bNumber, bDescription;
+        int i = 0;  // start at 1 because 0 is a filler in spinner object
+
+        // FIREBASE AND JSON
+        Gson gson = new Gson();
+        DatabaseReference mDatabaseRef;
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference();
+        Map<String, Object> map;
+
+
+        for(String s : allBuildingNames)
+        {
+            // skip first entry
+            if(i != 0) {
+                bNumber = s.split(" - ")[0];
+                bName = s.split(" - ")[1];
+                bDescription = "TO FILL IN";        // TODO - access another resource page and pass that in as another string array
+
+                newBuild = new Building(bNumber, bName, bDescription);
+                //allBuildings.add(newBuild);
+
+                //gson= new Gson();
+                String myJson = gson.toJson(newBuild);
+
+                map = new Gson().fromJson(myJson,
+                        new TypeToken<HashMap<String, Object>>() {
+                        }.getType());
+
+                mDatabaseRef.child("building").child(Integer.toString(i)).setValue(map);
+            }
+            i++;
+        }
+
+        //return allBuildings;
+    }
+
+
     // basic information return functions
-    public String getFullBuildingName() { return mBuildingName; }
+    public String getBuildingNumber() { return mBuildingNumber; }
+    public String getBuildingName() { return mBuildingName; }
     public String getBuildingDescription(){ return mBuildingDescription; }
     public LatLng getBuildingCenter(){ return mBuildingCenter; }
 
-    // mBuildingName is in always in format '[string] - [string]'
-    //      the first string contains the 'Building number'
-    //      the second string contains the 'Building name'
-    //  The following two functions grab each respective part
-    public String getBuildingNumber()
-    {
-        return mBuildingName.split(" - ")[0];
-    }
-
-    public String getBuildingName()
-    {
-        return mBuildingName.split(" - ")[1];
-    }
 
 
  // CLASSROOM RELATED FUNCTIONS
--- app/src/main/java/edu/calpoly/mjew/cpe436_calpolymapapp/ClassroomSelect.java
+++ app/src/main/java/edu/calpoly/mjew/cpe436_calpolymapapp/ClassroomSelect.java
@@ -1,6 +1,7 @@
 package edu.calpoly.mjew.cpe436_calpolymapapp;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.design.widget.FloatingActionButton;
@@ -10,16 +11,17 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
-import org.json.JSONObject;
-
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-
 
 public class ClassroomSelect extends AppCompatActivity {
 
@@ -48,27 +50,56 @@ public class ClassroomSelect extends AppCompatActivity {
                 //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         //.setAction("Action", null).show();
 
+                // if user is authenticated, allow classroom addition
                 if(FirebaseAuth.getInstance().getCurrentUser() != null)
                 {
-                    Gson gson = new Gson();
+
+                    Resources res = getResources();
+                    String[] allBuildings = res.getStringArray(R.array.buildings);
+
+                    // write test - FIREBASE AND JSON
+                    /*Gson gson = new Gson();
                     ClassRoom testClass = new ClassRoom(56, "this is a test class");
-                    //mDatabaseRef.child("classrooms").child(0)
-                            //.child().setValue(gson.toJson(testClass));
                     String myJson = gson.toJson(testClass);
+
                     Map<String, Object> map = new Gson().fromJson(myJson,
                             new TypeToken<HashMap<String, Object>>() {}.getType());
-                    mDatabaseRef.child("classroom").child("0").setValue(map);
+                    mDatabaseRef.child("classroom").child("0").setValue(map);*/
+
+                    // initialize database with buildings
+                    Building.InitializeAllBuildings(allBuildings);
+
+
+                    // read test - FIREBASE AND JSON
+                    mDatabaseRef.child("classroom").child("0").child("mDescription")
+                           .addValueEventListener(new ValueEventListener() {
+
+                            @Override
+                            public void onDataChange(DataSnapshot dataSnapshot) {
+                                updateTitle(dataSnapshot.getValue().toString());
+                            }
+
+                            @Override
+                            public void onCancelled(DatabaseError databaseError) {
+
+                            }
+                           });
                 }
                 else
                 {
                     // change to an option to log in with google?
-                    Snackbar.make(view, "Must be logged in to add Photos", Snackbar.LENGTH_LONG)
+                    Snackbar.make(view, "Must be logged in to add Classrooms", Snackbar.LENGTH_LONG)
                             .setAction("Action", null).show();
                 }
 
 
             }
         });
+    }
+
+    public void updateTitle(String newTitle)
+    {
+        setTitle(newTitle);
     }
 
 }
